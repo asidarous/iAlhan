f99b31ceb9ddaec48f02c94bd0195aa55ed11e64
Arsani Sidarous <asidarous@am.sony.com>
2017-04-02 15:40:53 -0700

    Added send email and adjusted contactVC elements

--- iAlhanNew/ContactVC.swift
+++ iAlhanNew/ContactVC.swift
@@ -7,20 +7,61 @@
 //
 
 import UIKit
+import MessageUI
 
-class ContactVC: UIViewController {
+class ContactVC: UIViewController, MFMailComposeViewControllerDelegate {
 
     @IBOutlet var nameField: UITextField!
 
     @IBOutlet var emailField: UITextField!
 
-    @IBOutlet var messageField: UITextView!
+    @IBOutlet var messageField: UITextField!
 
-    @IBAction func sendEmail(_ sender: Any) {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
     }
     
     
+    
+    
+    
+    @IBAction func sendEmail(_ sender: Any) {
+        
+        if MFMailComposeViewController.canSendMail() {
+        let toRecepients = ["arsani@alhan.org"]
+        let mc: MFMailComposeViewController = MFMailComposeViewController()
+        mc.mailComposeDelegate = self
+        
+        mc.setToRecipients(toRecepients)
+        mc.setSubject("iAlhan message from: \(nameField.text!)")
+        mc.setMessageBody("Name: \(nameField.text!)\n\n Email: \(emailField.text!) \n\n Message: \(messageField.text!)", isHTML: true)
+        
+        self.present(mc,animated: true, completion: nil)
+        }else {
+        
+            let sendMailErrorAlert = UIAlertController(title: "Could Not Send Email", message: "Your device could not send Email.  Please check Email configuration and try again.", preferredStyle: UIAlertControllerStyle.alert)
+            sendMailErrorAlert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+             self.present(sendMailErrorAlert, animated: true, completion: nil)
+            
+
+        }
+    }
+    
+    
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        controller.dismiss(animated: true)
+        self.dismiss(animated: true, completion: nil)
+    }
+
+    
+    @IBAction func dismissKeyboard(_ sender: Any) {
+        self.resignFirstResponder()
+    }
+    
     @IBAction func done(_ sender: AnyObject) {
         self.dismiss(animated: true, completion: nil)
+        
     }
 }
